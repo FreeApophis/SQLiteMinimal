379eb32d349a3d1f138e834dcef0efd52fc69233
Thomas Bruderer <apophis@apophis.ch>
2011-11-02 15:49:58 +0100

    Generate DBML

--- SQLiteMinimal/Database/Build.bat
+++ SQLiteMinimal/Database/Build.bat
@@ -1,1 +1,2 @@
+DbMetal.exe /provider:SQLite "/conn:Data Source=Example.s3db" /dbml:../Database.dbml /pluralize
 DbMetal.exe /namespace:Database /provider:SQLite "/conn:Data Source=Example.s3db" /code:../Database.cs /pluralize
